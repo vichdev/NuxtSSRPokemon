e6200310c4cd8bc9a3f3b82ac36e95f0014a4ae2
VictorHugo <victor.hugo.antunes.n@gmail.com>
2023-02-10 11:58:14 -0300

    feat: nuxt ssr pokemon app test

---
+++ src/pages/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <Tutorial :pokemon="pokemon" />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import API from "../services/api";
+
+interface IPokemon {
+  name: string;
+  sprites: {
+    front_default: string;
+  };
+  height: number;
+  weight: number;
+}
+
+export default Vue.extend({
+  name: "IndexPage",
+  data() {
+    return {
+      pokemon: {} as Partial<IPokemon>,
+    };
+  },
+  head() {
+    return {
+      title: this.pokemon?.name?.toUpperCase(),
+      meta: [
+        {
+          hid: "title",
+          name: "title",
+          property: "og:title",
+          content: `${this.pokemon?.name?.toUpperCase()}`,
+        },
+        {
+          hid: "image",
+          name: "image",
+          property: "og:image",
+          content: `${this.pokemon?.sprites?.front_default}`,
+        },
+        {
+          hid: "description",
+          name: "description",
+          property: "og:description",
+          content: `Informação detalhada sobre o pokemon ${this.pokemon?.name}`,
+        },
+      ],
+    };
+  },
+  computed: {
+    nomeDePokemonAleatorio() {
+      const nomes = [
+        "ditto",
+        "pikachu",
+        "charizard",
+        "squirtle",
+        "charmander",
+        "beedrill",
+        "lugia",
+      ];
+      return nomes[Math.floor(Math.random() * nomes.length)];
+    },
+  },
+
+  methods: {
+    async getPokemon() {
+      await API.get(this.nomeDePokemonAleatorio, {})
+        .then((res) => {
+          console.log(res?.data);
+          this.pokemon = res?.data;
+        })
+        .catch((e) => console.log(e));
+    },
+  },
+  async beforeMount() {
+    await this.getPokemon();
+  },
+});
+</script>
